307de60412af969921c066ecf7dd7a9c7811bb59
vlad <vladov3000@gmail.com>
2017-04-30 16:43:54 -0700

    a

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,1 +1,1 @@
-docker run -v ~/minecraftServer:/minecraftServer -p 25565:25565 -it minecraft-srv /bin/sh
+docker run -v ~/minecraftServer:/minecraftServer -p 25565:25565 -it minecraft-srv /bin/sh -c 'cd /minecraftServer;./start_server.sh'
